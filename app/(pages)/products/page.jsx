8cacab34a65f0003bf489e092b36cf4502f696fa
silencieux99 <dev.alamiya@gmail.com>
2025-12-15 03:37:11 +0100

    Fix: Move use client directive to first line

--- app/(pages)/products/page.jsx
+++ app/(pages)/products/page.jsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
+
+export const dynamic = 'force-dynamic';
 
 import { useEffect, useState, useMemo } from 'react';
 import { collection, query, where, orderBy, onSnapshot } from 'firebase/firestore';
